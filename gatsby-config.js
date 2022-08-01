706a59119b3193d07cd3cad9c01a6c17eae6c7d2
emontes <emontes@turista.com.mx>
2022-07-31 20:46:10 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,26 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-postcss',
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
     },
-    __key: "images"
-  }]
-};
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'blog-posts',
+        path: `${__dirname}/src/blog-posts`,
+      },
+    },
+  ],
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false,
+  content: [],
+  theme: {
+    fontFamily: {
+      montserrat: ['Montserrat, san-serif'],
+      rammetto: ['Rammetto One, cursive'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
